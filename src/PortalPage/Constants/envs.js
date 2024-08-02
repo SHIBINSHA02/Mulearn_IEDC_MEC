7a3748123f3e7c096de3ef9c2ee719369683a534
Nalin <79359909+NotNalin@users.noreply.github.com>
2024-08-02 20:58:25 +0530

    Fix error

--- src/PortalPage/Constants/envs.js
+++ src/PortalPage/Constants/envs.js
@@ -1,3 +1,3 @@
 
-export const { REACT_APP_SERVER_URL } = process.env;
-
+// export const { REACT_APP_SERVER_URL } = process.env;
+export const { REACT_APP_SERVER_URL } = "example.com"; // change later
