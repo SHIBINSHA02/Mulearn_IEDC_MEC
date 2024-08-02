266ff7d7a40b0f4269425b1c583a0d21e012aecf
Janukrishna A S <janukrishnaas@gmail.com>
2024-08-02 21:48:48 +0530

    tem data trial

--- src/Components/ExploreLC/ExploreLC.tsx
+++ src/Components/ExploreLC/ExploreLC.tsx
@@ -8,7 +8,7 @@ const ExploreLC = () => {
                 <img src={image} alt="" />
             </div>
             <div className={styles.contentWrapper}>
-                <h1>Explore Learning Circles</h1>
+                <h1>Explore our communities </h1>
                 <p>
                     Join IEDC today itself to enhance your new ideas
                 </p>
