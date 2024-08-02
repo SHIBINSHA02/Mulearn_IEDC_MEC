df17fc5eedb92e0073328c72c878ebbb853637e4
Janukrishna A S <janukrishnaas@gmail.com>
2024-08-02 23:15:08 +0530

    portal cc

--- src/Components/ExploreLC/ExploreLC.tsx
+++ src/Components/ExploreLC/ExploreLC.tsx
@@ -12,12 +12,19 @@ const ExploreLC = () => {
                 <p>
                     Join IEDC today itself to enhance your new ideas
                 </p>
-                <a target="_blank" href="https://forms.fillout.com/t/e15cUVRMRVus">
+                {
+                    /*<a target="_blank" href="https://forms.fillout.com/t/e15cUVRMRVus">
                     IEDC portal
                 </a>
+                */}
+                <div data-fillout-id="e15cUVRMRVus" data-fillout-embed-type="popup" data-fillout-dynamic-resize data-fillout-inherit-parameters data-fillout-popup-size="medium"> 
+                </div>
+                <script src="https://server.fillout.com/embed/v1/">
+                </script>
             </div>
         </div>
     );
 };
 
 export default ExploreLC;
+
